ecb56dfe9a6951c05de99a00d536e0ba50716f61
Vido <vidoseaver@gmail.com>
2016-12-14 11:18:38 -0700

    fixed zipcode spelling

--- app/services/fuel_station_service.rb
+++ app/services/fuel_station_service.rb
@@ -1,4 +1,3 @@
-require '*/services/'
 class  FuelStationService
   def self.find_electric_stations_within_6(zipcode)
     url  =  "http://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV['nrel_api_key']}&fuel_type=ELEC&location=#{zipcode}&limit=10"
@@ -6,7 +5,7 @@ class  FuelStationService
     electric_response = Net::HTTP.get(uri)
     FuelStation.parse_and_create(electric_response)
   end
-  def self.find_propane_stations_within_6(zipocode)
+  def self.find_propane_stations_within_6(zipcode)
     url  =  "http://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV['nrel_api_key']}&fuel_type=LPG&location=#{zipcode}&limit=10"
     uri = URI(url)
     electric_response = Net::HTTP.get(uri)
