3a2ba3c77fd7d31586dd4abc79a0729d86f49d92
Vido <vidoseaver@gmail.com>
2016-12-14 10:10:54 -0700

    faraday request working

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
   def index
     zipcode = params[:q]
-     Faraday.get()
+    @electric_stations = Faraday.get("http://developer.nrel.gov/api/alt-fuel-stations/v1.json?api_key=#{ENV['nrel_api_key']}&fuel_type=ELEC&zip=#{zipcode}&limit=10")
   end
 end
