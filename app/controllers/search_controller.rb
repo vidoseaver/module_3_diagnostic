2edc56f2c284751f4e9d57c8bdae81f6d27933e9
Vido <vidoseaver@gmail.com>
2016-12-14 11:05:36 -0700

    base functionality for propane stations

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,7 +5,14 @@ class SearchController < ApplicationController
       url  =  "http://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV['nrel_api_key']}&fuel_type=ELEC&location=#{zipcode}&limit=10"
       uri = URI(url)
       electric_response = Net::HTTP.get(uri)
+
+      url  =  "http://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV['nrel_api_key']}&fuel_type=LPG&location=#{zipcode}&limit=10"
+      uri = URI(url)
+      propane_response = Net::HTTP.get(uri)
+
+
       @electric_stations = FuelingStation.parse_and_create(electric_response)
+      @gas_stations =  FuelingStation.parse_and_create(propane_response)
     end
   end
 end
